672382916345b0990c7a8f0d9892a5be0ed0a3d5
Cepraga Mihail <yahim09@gmail.com>
2023-01-13 11:13:39 +0200

    Using Mokito annotations and clean code

--- src/test/java/net/tutorial/springboottesting/service/EmployeeServiceTests.java
+++ src/test/java/net/tutorial/springboottesting/service/EmployeeServiceTests.java
@@ -7,34 +7,35 @@ import net.tutorial.springboottesting.service.impl.EmployeeServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import static org.mockito.BDDMockito.given;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class EmployeeServiceTests {
 
+  @Mock
   private EmployeeRepository employeeRepository;
-  private EmployeeService employeeService;
+  @InjectMocks
+  private EmployeeServiceImpl employeeService;
+
+  Employee employee;
 
   @BeforeEach
   public void setup() {
-    employeeRepository = Mockito.mock(EmployeeRepository.class);
-    employeeService = new EmployeeServiceImpl(employeeRepository);
+    employee = Employee.builder().id(1L).firstName("Mihail").lastName("Cepraga").email("mihail@mail.com").build();
   }
 
   // JUnit test for
   @Test
   public void givenEmployeeObject_whenSaveEmployee_thenReturnEmployeeObject() {
     // given - precondition or setup
-    Employee employee = Employee.builder()
-        .id(1L)
-        .firstName("Mihail")
-        .lastName("Cepraga")
-        .email("mihail@mail.com")
-        .build();
-    BDDMockito.given(employeeRepository.findByEmail(employee.getEmail()))
-        .willReturn(Optional.empty());
-    BDDMockito.given(employeeRepository.save(employee))
-        .willReturn(employee);
+    given(employeeRepository.findByEmail(employee.getEmail())).willReturn(Optional.empty());
+    given(employeeRepository.save(employee)).willReturn(employee);
 
     // when - action or the behaviour that we are going test
     Employee saveEmployee = employeeService.saveEmployee(employee);
@@ -42,8 +43,5 @@ public class EmployeeServiceTests {
     // then - verify the output
     Assertions.assertThat(saveEmployee).isNotNull();
     Assertions.assertThat(saveEmployee.getId()).isEqualTo(employee.getId());
-
   }
-
-
 }
