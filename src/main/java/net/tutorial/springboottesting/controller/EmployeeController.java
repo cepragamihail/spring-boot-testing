59b4f4225ab8c8dbea4bea10b72b52a6371253ba
Cepraga Mihail <yahim09@gmail.com>
2023-01-21 12:34:51 +0200

    Create getAllEmployees EndPoint

--- src/main/java/net/tutorial/springboottesting/controller/EmployeeController.java
+++ src/main/java/net/tutorial/springboottesting/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package net.tutorial.springboottesting.controller;
 
+import java.util.List;
 import net.tutorial.springboottesting.model.Employee;
 import net.tutorial.springboottesting.service.EmployeeService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,11 @@ public class EmployeeController {
 
   public EmployeeController(EmployeeService employeeService) {
     this.employeeService = employeeService;
+  }
+
+  @GetMapping
+  public List<Employee> getAllEmployees() {
+    return employeeService.getAllEmployees();
   }
 
   @PostMapping
